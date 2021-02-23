0c7894e3ffa8211ce9d24394993c7560afda7581
Niharika2404 <niharikang24@gmail.com>
2021-02-23 21:56:54 +0530

    Daily Wage and Part time wage is updated

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -5,13 +5,14 @@ public class EmpWageBuilderUC1 {
 		int IS_FULL_TIME = 1;
 		int WAGE_PER_HR = 20;
 		int FULL_DAY_HR = 8;
+		int PART_TIME_HR = 0;
 		double DAILY_PAYMENT = 0;
 
 				double empCheck = Math.floor(Math.random() * 10) % 2;
 			if (empCheck == IS_FULL_TIME)
 					{
 					System.out.println("Employee is Present");
-					System.out.println("DAILY_PAYMENT " + WAGE_PER_HR * FULL_DAY_HR);
+					System.out.println("DAILY_PAYMENT " + WAGE_PER_HR * (FULL_DAY_HR + PART_TIME_HR));
 					}
 
 			else
