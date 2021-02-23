8e7350466e6d8dda2c4177dbb3ebb94450b79ba7
Niharika2404 <niharikang24@gmail.com>
2021-02-23 21:17:51 +0530

    Daily Payment is updated.

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -3,10 +3,21 @@ public class EmpWageBuilderUC1 {
 	public static void main(String[] args)
 	{
 		int IS_FULL_TIME = 1;
+		int WAGE_PER_HR = 20;
+		int FULL_DAY_HR = 8;
+		double DAILY_PAYMENT = 0;
+
 			double empCheck = Math.floor(Math.random() * 10) % 2;
 			if (empCheck == IS_FULL_TIME)
-					System.out.ptintln( "Employee is Present" );
+					{
+					System.out.println("Employee is Present");
+					System.out.println("DAILY_PAYMENT " + WAGE_PER_HR * FULL_DAY_HR);
+					}
+
 			else
-					System.out.println( "Employee is absent" );
+					{
+					System.out.println("Employee is absent");
+					System.out.println("DAILY_PAYMENT " + DAILY_PAYMENT);
+					}
 	}
 }
