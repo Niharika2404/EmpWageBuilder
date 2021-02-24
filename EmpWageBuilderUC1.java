08e7bf6d46572c197e9d99f4e8357679b7d1b9cc
Niharika2404 <niharikang24@gmail.com>
2021-02-24 14:01:17 +0530

    Switch case is updated

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -2,23 +2,26 @@ public class EmpWageBuilderUC1 {
 
 	public static void main(String[] args)
 	{
-		int IS_FULL_TIME = 1;
 		int WAGE_PER_HR = 20;
 		int FULL_DAY_HR = 8;
 		int PART_TIME_HR = 0;
 		double DAILY_PAYMENT = 0;
 
 				double empCheck = Math.floor(Math.random() * 10) % 2;
-			if (empCheck == IS_FULL_TIME)
-					{
-					System.out.println("Employee is Present");
-					System.out.println("DAILY_PAYMENT " + WAGE_PER_HR * (FULL_DAY_HR + PART_TIME_HR));
-					}
-
-			else
-					{
-					System.out.println("Employee is absent");
-					System.out.println("DAILY_PAYMENT " + DAILY_PAYMENT);
-					}
+			switch (empCheck)
+				{
+					case FULL_DAY_HR:
+							System.out.println("Employee is present");
+							DAILY_PAYMENT = FULL_DAY_HR * WAGE_PER_HR;
+							break;
+					case PART_TIME_HR:
+							System.out.println("Employee is Present");
+							DAILY_PAYMENT = FULL_DAY_HR * WAGE_PER_HR * PART_TIME_HR;
+							break;
+					default:
+							System.out.println("Employee is absent");
+							break;
+				}
+				System.out.println("Daily Payment: " + DAILY_PAYMENT);
 	}
 }
